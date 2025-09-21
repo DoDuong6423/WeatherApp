cfbada1344e6529e3d27b653fff228cb697ee79f
DoDuong6423 <dovanthaiduong6423@gmail.com>
2025-09-21 22:46:32 +0700

    https

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -4,7 +4,7 @@ export default function SearchSection({ getWeatherDetails, searchInputRef }) {
   const handleCitySearch = (e) => {
     e.preventDefault();
     const searchInput = e.target.querySelector(".search-input");
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
     getWeatherDetails(API_URL); // Lấy thông tin thời tiết cho thành phố đã nhập
   };
 
@@ -13,7 +13,7 @@ export default function SearchSection({ getWeatherDetails, searchInputRef }) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
-        const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
+        const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${latitude},${longitude}&days=2`;
         getWeatherDetails(API_URL); // Lấy thông tin thời tiết tại vị trí hiện tại của người dùng
         window.innerWidth >= 768 && searchInputRef.current.focus();
       },
